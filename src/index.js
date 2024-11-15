cb13645a1170e5f89ba08c068c280ac930835425
Keval Maru <82196891+kevalmaru-gpu@users.noreply.github.com>
2024-11-15 22:18:58 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,14 +2,7 @@ require('dotenv').config({ path: '../.env' });
 const { app, logger } = require('./app');
 const config = require('./config');
 const http = require('http');
-const Redis = require('./utils/lib/redis');
-const CockroachDb = require('./utils/lib/cockroachdb');
-const MongoDb = require('./utils/lib/mongodb');
 const server = http.createServer(app);
-
-(async () => {
-    const db = new MongoDb('')
-})()
 
 server.listen(config.service.port, config.service.host, () => {
   logger.info(`Server started at ${config.service.host}:${config.service.port}`);
